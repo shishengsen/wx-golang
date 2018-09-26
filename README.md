2878e150ae5b46db9469eca71f41288c88c56d06
liaochuntao <liaochuntao@live.com>
2018-09-26 23:13:45 +0800

    修复部分错误

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Golang 实现的微信公众号开发工具
 
-![License](https://img.shields.io/github/license/Weixin-Golang/wx-golang.svg) 
+![License](https://img.shields.io/github/license/Weixin-Golang/wx-golang.svg)
 ![stars](https://img.shields.io/github/stars/Weixin-Golang/wx-golang.svg)
 ![forks](https://img.shields.io/github/forks/Weixin-Golang/wx-golang.svg)
 ![building](https://img.shields.io/badge/status-building-green.svg?longCache=true&style=plastic)
@@ -14,6 +14,7 @@
 - [x] 微信自定义菜单相关操作的实现
 - [x] 微信客服相关接口
 - [x] 微信消息通知的路由分发操作实现
+- [x] `http`接口实现
 
 #### TODO
 
--- weixin-mp/service/WxMsgRouterService.go
+++ weixin-mp/service/WxMsgRouterService.go
@@ -4,6 +4,17 @@ import (
 	"weixin-golang/weixin-mp/enpity"
 )
 
+const (
+	MSG_TYPR_TEXT			=		"text"
+	MSG_TYPE_IMAGE			=		"image"
+	MSG_TYPE_VOICE			=		"voice"
+	MSG_TYPE_VIDEO			=		"video"
+	MSG_TYPE_SHORT_VIDEO	=		"shortvideo"
+	MSG_TYPE_LOCATION		=		"location"
+	MSG_TYPE_LINK			=		"link"
+	MSG_TYPE_EVENT			=		"event"
+)
+
 var handlerMap map[string]*enpity.MsgRouter
 
 // 微信消息通知路由分发
@@ -11,8 +22,11 @@ func (w *WeChat) RouterInit(msgRouters map[string]*enpity.MsgRouter) {
 	handlerMap = msgRouters
 }
 
-// 路由分发
+// 路由分发，
 func (w *WeChat) RouterSwitchHandler(msg enpity.WxMessage) {
+	if &handlerMap == nil {
+		panic("微信消息路由对象为空")
+	}
 	msgType := msg.MsgType
 	router := handlerMap[msgType]
 	(*router).Handler(msg)
