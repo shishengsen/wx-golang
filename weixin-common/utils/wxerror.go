6fcdf80c930ad3e320f7c6d37c5aaef6ea6d807c
liaochuntao <liaochuntao@live.com>
2018-10-07 23:32:12 +0800

    (create)增加微信错误工具类

--- weixin-common/utils/wxerror.go
+++ weixin-common/utils/wxerror.go
@@ -1,6 +1,24 @@
 package utils
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // 对微信返回信息进行错误信息扫描，如果发现非正常状态返回，则抛出异常信息
-func WxError(result []byte) []byte {
+func WxMpError(result []byte, resp *http.Response) []byte {
+	var tmp map[string]string
+	err := json.Unmarshal(result, &tmp)
+	if err != nil {
+		panic(err)
+	}
+	if _, ok := tmp["errcode"]; ok == true {
+		panic(fmt.Errorf("微信调用api报错：%#v", tmp))
+	}
+	return result
+}
+
+func WxPayError(result []byte, resq *http.Response) []byte {
 	return nil
 }
